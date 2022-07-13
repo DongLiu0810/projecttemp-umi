3ea29c059fc7f7984a41db23cbb9fe063b59f2e0
liudong <liudong@hyperchain.cn>
2022-07-13 15:51:54 +0800

    feat(dist): 去除打包产物

--- umiTemp/p__home.83eafc83.async.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp_projectTemp"]=window["webpackJsonp_projectTemp"]||[]).push([[2],{FCkD:function(n,e,r){"use strict";r.r(e),r.d(e,"default",(function(){return o}));r("q1tI");var t=r("U0+B"),c=r.n(t),p=r("nKUr");function o(){return Object(p["jsx"])("div",{className:c.a.wrapper,children:"\u4e3b\u9875"})}},"U0+B":function(n,e,r){}}]);
